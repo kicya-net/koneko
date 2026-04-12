83851976ba3e182898b17aef8c935f77ffec8ab0
dimden <admin@dimden.dev>
2026-04-12 16:29:37 +0200

    nicer prefix

--- src/api/sandbox/prefix.js
+++ src/api/sandbox/prefix.js
@@ -1,51 +1,35 @@
-let request;
-{
-    const __kWrapFormFile = (file) => {
-        if(!file || typeof file !== 'object' || !file._ref) return file;
-        return {
-            name: file.name,
-            mimetype: file.mimetype,
-            size: file.size,
-            read: () => file._ref.copySync(),
-            text: () => file._textRef.applySync(undefined, [], {
-                arguments: { copy: true },
-                result: { copy: true },
-            }),
-            json: () => JSON.parse(file._textRef.applySync(undefined, [], {
-                arguments: { copy: true },
-                result: { copy: true },
-            })),
-        };
+if(__request?.body?.type === 'form-data' && __request?.body?.files) {
+    for(const fieldName in __request.body.files) {
+        const files = __request.body.files[fieldName];
+        for(let i = 0; i < files.length; i++) {
+            const file = files[i];
+            files[i] = {
+                name: file.name,
+                mimetype: file.mimetype,
+                size: file.size,
+                read: () => file._ref.copySync(),
+                text: () => file._textRef.applySync(undefined, [], {
+                    arguments: { copy: true },
+                    result: { copy: true },
+                }),
+                json: () => JSON.parse(file._textRef.applySync(undefined, [], {
+                    arguments: { copy: true },
+                    result: { copy: true },
+                })),
+            };
+        }
     }
-
-    const __kWrapFormFiles = (files) => {
-        return Object.fromEntries(
-            Object.entries(files ?? {}).map(([key, value]) => [
-                key,
-                (Array.isArray(value) ? value : [value]).map(__kWrapFormFile),
-            ]),
-        );
-    }
-
-    const __kWrapRequestBody = (body) => {
-        if(!body || typeof body !== 'object') return body;
-        if(body.type !== 'form-data') return body;
-        return {
-            ...body,
-            files: __kWrapFormFiles(body.files),
-        };
-    }
-
-    request = { 
-        url: __request.url,
-        path: __request.path,
-        method: __request.method,
-        headers: new Headers(__request.headers),
-        body: __kWrapRequestBody(__request.body),
-        query: __request.query,
-        cookies: __request.cookies,
-    };
 }
+
+const request = { 
+    url: __request.url,
+    path: __request.path,
+    method: __request.method,
+    headers: new Headers(__request.headers),
+    body: __request.body,
+    query: __request.query,
+    cookies: __request.cookies,
+};
 const __k = [];
 const response = { status: 200, statusText: '', headers: new Headers() };
 function echo(v) { __k.push(v); }
